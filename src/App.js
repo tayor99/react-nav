92d73ae7f58bb662849b6961aa6553ae71c34af6
Adenusi Adetayo <adenusiadetayo@gmail.com>
2022-05-09 16:57:47 +0100

    third

--- src/App.js
+++ src/App.js
@@ -3,46 +3,42 @@ import logo from './Image/logo.png';
 import './App.css';
 
 function App() {
-  const [menu, setMenu] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const handleMenu = () => {
-    setMenu(!menu);
-  };
-
-  const newClass = menu ? 'hamLink' : '';
-
-  console.log(newClass);
-
-  console.log(menu);
+  const newClass = isMenuOpen ? 'hamLink' : '';
 
   return (
-    <div className={`App ${menu ? 'active' : 'not-active'}`}>
+    <header className={`App ${isMenuOpen ? 'active' : 'not-active'}`}>
       <div className="logo">
         <img src={logo} alt="logo png" />
       </div>
       <div
-        className={`hamburger ${menu ? 'open' : 'close'}`}
-        onClick={handleMenu}
+        className={`hamburger ${isMenuOpen ? 'open' : 'close'}`}
+        onClick={() => {
+          setIsMenuOpen((isMenuOpen) => !isMenuOpen);
+        }}
       >
         <div className="burger burger1"></div>
         <div className="burger burger2"></div>
         <div className="burger burger3"></div>
       </div>
-      <ul className={newClass}>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="#about">About Us</a>
-        </li>
-        <li>
-          <a href="#contact">Contact Us</a>
-        </li>
-        <li>
-          <a href="#more">More</a>
-        </li>
-      </ul>
-    </div>
+      <nav>
+        <ul className={newClass}>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="#about">About Us</a>
+          </li>
+          <li>
+            <a href="#contact">Contact Us</a>
+          </li>
+          <li>
+            <a href="#more">More</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
